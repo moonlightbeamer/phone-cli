60cc155530c877f50fa035695d07b548d1e5ef1d
mtsouk <mihalistsoukalos@gmail.com>
2021-03-24 09:39:25 +0200

    Minor change

--- cmd/search.go
+++ cmd/search.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2021 Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
-
 */
+
 package cmd
 
 import (
@@ -13,13 +13,8 @@ import (
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Search for a telephone number",
+	Long:  `This command searches for a given phone number..`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("search called")
 	},
@@ -28,13 +23,5 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(searchCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	searchCmd.Flags().StringP("tel", "t", "", "Telephone number to search")
 }
